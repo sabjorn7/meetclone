e54d78411dd6de72885e7548e065cb75307ff6ef
Arseny Sazhin (via WeWeb) <sazhinarseny@outlook.com>
2025-07-21 10:28:59 +0300

    v95 - delay

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 94;
+const version = 95;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);

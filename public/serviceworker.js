7942d627fc74a191e9367dbfc6eed952220dce37
Arseny Sazhin (via WeWeb) <sazhinarseny@outlook.com>
2025-08-01 13:31:08 +0300

    v107 - deploy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 106;
+const version = 107;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);

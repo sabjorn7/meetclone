7b327ebabe371a67f376a00c11e1c91f6d96944a
Arseny Sazhin (via WeWeb) <sazhinarseny@outlook.com>
2025-08-25 18:26:07 +0300

    v167 - courses

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 166;
+const version = 167;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);

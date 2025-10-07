6509a8bcc3584ffedc4d0a96ce6f256cc9005114
Arseny Sazhin (via WeWeb) <sazhinarseny@outlook.com>
2025-10-07 18:42:35 +0300

    v197 - regis

--- src/pages/75f669f5-a96d-454c-8965-38fcf8a6a7a2.js
+++ src/pages/75f669f5-a96d-454c-8965-38fcf8a6a7a2.js
@@ -14,6 +14,7 @@ import element_b783dc65_d528_4f74_8c14_e27c934c39b1 from '@/components/elements/
 import element_c6c0c00e_49fd_4cb9_bd78_5bc09945721e from '@/components/elements/element-c6c0c00e-49fd-4cb9-bd78-5bc09945721e/src/wwElement.vue';
 import element_d7904e9d_fc9a_4d80_9e32_728e097879ad from '@/components/elements/element-d7904e9d-fc9a-4d80-9e32-728e097879ad/src/wwElement.vue';
 import element_deb10a01_5eef_4aa1_9017_1b51c2ad6fd0 from '@/components/elements/element-deb10a01-5eef-4aa1-9017-1b51c2ad6fd0/src/wwElement.vue';
+import element_eb4b02a3_cbe6_47e9_8db5_322da8047160 from '@/components/elements/element-eb4b02a3-cbe6-47e9-8db5-322da8047160/src/wwElement.vue';
 
 // eslint-disable-next-line no-undef
 app.component('wwobject-14723a21-0178-4d92-a7e9-d1dfeaec29a7', element_14723a21_0178_4d92_a7e9_d1dfeaec29a7);
@@ -28,6 +29,7 @@ app.component('wwobject-b783dc65-d528-4f74-8c14-e27c934c39b1', element_b783dc65_
 app.component('wwobject-c6c0c00e-49fd-4cb9-bd78-5bc09945721e', element_c6c0c00e_49fd_4cb9_bd78_5bc09945721e);
 app.component('wwobject-d7904e9d-fc9a-4d80-9e32-728e097879ad', element_d7904e9d_fc9a_4d80_9e32_728e097879ad);
 app.component('wwobject-deb10a01-5eef-4aa1-9017-1b51c2ad6fd0', element_deb10a01_5eef_4aa1_9017_1b51c2ad6fd0);
+app.component('wwobject-eb4b02a3-cbe6-47e9-8db5-322da8047160', element_eb4b02a3_cbe6_47e9_8db5_322da8047160);
 
 // eslint-disable-next-line no-undef
 import section_99586bd3_2b15_4d6b_a025_6a50d07ca845 from '@/components/sections/section-99586bd3-2b15-4d6b-a025-6a50d07ca845/src/wwSection.vue';

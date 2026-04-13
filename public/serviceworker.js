aed0484d9ef33dcc15ca84de00041e1a598a7ead
Arseny Sazhin (via WeWeb) <sazhinarseny@outlook.com>
2026-04-13 17:11:44 +0300

    v248 - sitemap

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 246;
+const version = 248;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
